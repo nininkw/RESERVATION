884017d25d606f727f2e5236ad57a06657e9ce19
nininkw <hraktai012ka@gmail.com>
2022-09-03 14:10:30 +0900

    提出２回目マイグレーションファイルの修正

--- db/migrate/20220724044540_remove_days_from_reservations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDaysFromReservations < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reservations, :Days, :integer
-  end
-end
--- db/migrate/20220724044559_remove_total_fee_from_reservations.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTotalFeeFromReservations < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :reservations, :Total_fee, :integer
-  end
-end
